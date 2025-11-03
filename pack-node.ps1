25fee98f94ec5913f52aa968727a82fcc322ed4b
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2025-11-03 09:23:28 -0800

    Update pack-node.ps1

--- pack-node.ps1
+++ pack-node.ps1
@@ -3,8 +3,9 @@ $ErrorActionPreference = 'Stop'
 $avvmRoot = 'c:\avvm\node'
 
 $node_versions = @(
-    "22.18.0",
-    "23.11.1"
+    "22.21.1",
+    "24.11.0",
+    "25.1.0"
 )
 
 foreach ($node_version in $node_versions) {
